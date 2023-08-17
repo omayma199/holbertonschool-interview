dffaa04c519cacb369c481c7bc04a5c90a526d7f
oumaimamizouni <mizounioumaima@gmail.com>
2023-08-17 10:59:26 +0100

    lockboxes

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,15 +1,29 @@
 #!/usr/bin/python3
+
+#Task 0: Lockboxes
+
+
 def canUnlockAll(boxes):
-    n = len(boxes)
-    visited = [False] * n
-    visited[0] = True
-    stack = [0]
-
+    # Total number of boxes
+    n = len(boxes)  
+    # Initialize a list to track visited boxes
+    visited = [False] * n  
+    # Mark the first box as visited
+    visited[0] = True  
+    # Start the stack with the first box
+    stack = [0]  
+    
+   
     while stack:
-        current_box = stack.pop()
-        for key in boxes[current_box]:
-            if key >= 0 and key < n and not visited[key]:
-                visited[key] = True
-                stack.append(key)
-
-    return all(visited)
+        # Get the current box from the stack
+        current_box = stack.pop()  
+        # Iterate through keys in the current box
+        for key in boxes[current_box]:  
+            # Check if key is valid and not visited
+            if key >= 0 and key < n and not visited[key]:  
+                # Mark the box as visited
+                visited[key] = True  
+                # Add the box to the stack for further exploration
+                stack.append(key)  
+    # Return True if all boxes are visited, otherwise False
+    return all(visited)  
